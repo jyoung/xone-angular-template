2a724f18eb6e04a031e2a4a04a81ec013f1741a9
Joe Young <joe.young@expresspros.com>
2020-03-20 09:15:08 -0500

    updated material module

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -5,73 +5,43 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatBadgeModule,
     MatListModule,
     MatGridListModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatSelectModule,
     MatRadioModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatChipsModule,
-    MatTooltipModule,
-    MatTableModule,
     MatPaginatorModule,
     MatCardModule,
-    MatCheckboxModule,
-    MatMenuModule,
-    MatExpansionModule
+    MatCheckboxModule
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatBadgeModule,
     MatListModule,
     MatGridListModule,
-    MatInputModule,
-    MatFormFieldModule,
     MatSelectModule,
     MatRadioModule,
-    MatDatepickerModule,
-    MatChipsModule,
-    MatTooltipModule,
-    MatTableModule,
     MatPaginatorModule,
     MatCardModule,
     MatCheckboxModule,
-    MatMenuModule,
-    MatExpansionModule
   ],
   providers: [
-    MatDatepickerModule,
   ]
 })
 
