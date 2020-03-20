e5a1ffb6d190909dc9adb580590daabc6d157478
Joe Young <joe.young@expresspros.com>
2020-03-20 09:29:31 -0500

    it compiles!

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,6 +13,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   imports: [
@@ -26,7 +28,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatRadioModule,
     MatPaginatorModule,
     MatCardModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
     MatButtonModule,
@@ -40,6 +44,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatPaginatorModule,
     MatCardModule,
     MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [
   ]
